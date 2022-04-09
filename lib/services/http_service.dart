2182975f3b390d1bceaa773d2290cde3963a9a18
cjmilner73 <chris@blueemoon.sg>
2022-04-10 00:40:00 +0800

    Automatic Commit

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -46,12 +46,33 @@ class HttpService {
       throw "Can't get posts.";
     }
   }
-
-  Future<List<Post>> getOCHL() async {
+  Future<List<Post>> getOHCL() async {
     String postsUrl = formURL();
     Response res = await get(Uri.parse(postsUrl));
     if (res.statusCode == 200) {
       var lists = json.decode(res.body);
       List<Post> list = [];
       Post p = Post(id: '', price: 0.0, amount: 0, total: 0, day_change: 0.0);
+      for (var i = 0; i < lists.length; i++) {
+        String thisId = lists[i][1];
+        double thisPrice = lists[i][3];
+        int thisAmount = lists[i][2];
+        double thisDayChange = lists[i][4];
+        String newThisDayChange = thisDayChange.toStringAsFixed(3);
+        p = Post(
+            id: thisId,
+            price: thisPrice,
+            amount: thisAmount,
+            total: thisAmount * thisPrice,
+            day_change: thisDayChange);
+        list.add(p);
+        list.sort((a, b) => b.total.compareTo(a.total));
+      }
+      List newList = [1,1,3,4];
+      // return newList;
+      return list;
+    } else {
+      throw "Can't get posts.";
+    }
+  }
 }
