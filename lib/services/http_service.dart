170c85ddfd46c173425de56b09881571d7e0626c
cjmilner73 <chris@blueemoon.sg>
2023-01-17 20:18:02 +0800

    Automatic Commit

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -30,6 +30,7 @@ class HttpService {
       List<Post> list = [];
       Post p = Post(id: '', price: 0.0, amount: 0, total: 0, day_change: 0.0);
       for (var i = 0; i < lists.length; i++) {
+        // String thisId = lists[i][1];
         String thisId = lists[i][1];
         double thisPrice = lists[i][3];
         int thisAmount = lists[i][2];
